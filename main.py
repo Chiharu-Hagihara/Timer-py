d135417176e7ff49a4c05dbb13e383e9d4bf5af0
M-E-C <Mr_El_Capitan@outlook.jp>
2020-05-03 10:07:55 +0900

    １０秒カウントダウン

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from time import sleep
 
-sleep(10)
-print("１０秒経過しました。")
+target_time = 10
+
+
+def down_timer(secs):
+    # for i in range(0,secs):から変更
+    for i in range(secs, -1, -1):
+        print(i)
+        sleep(1)
+    print("時間です！")
+
+
+down_timer(target_time)
